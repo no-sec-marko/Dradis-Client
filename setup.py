7973710ba6d6a2c455da1cb508ccfb6b14f23927
Shane Scott <sscott@gvit.com>
2018-08-10 14:01:57 -0500

    Revise organization for PyPi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/GoVanguard/pydradis3",
-    packages=setuptools.find_packages(),
+    packages=['pydradis3'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: GPLv2 License",
